be118c49d8e02be80ba2aefa23b90b7a2b7e8d4e
S3yll <aubane.lecorchet@gmail.com>
2020-06-09 17:45:37 +0200

    Update script.js

--- front/js/script.js
+++ front/js/script.js
@@ -22,13 +22,11 @@ function loadRunners (runners) {
 
     /*
     var text="";
-    //var name = document.getElementById('name');
     runners.forEach(function (element) {
         console.log(element);
         text = '<div class="fiche-coureur" id="fiche-coureur-modele"><div class="first-row"><span class="nom-coureur">'+element['nom']+' '+element['prenom']+'</span><span class="categorie-coureur">(<span class="categorie-coureur-value">'+element['categorie_categorie_valeur']+'</span>)</span><button class="modifier-coureur" id="modify">modifier</button></div><div class="second-row"><div><img class="icon-coureur" src="../img/mail.png"><span class="info-coureur">'+element['mail']+'</span><div class="right-panel"><span class="info-italic-coureur">Numéro Licence: </span><span class="info-italic-coureur-value" id="licence">'+element['num_licence']+'</span></div></div><div><img class="icon-coureur" src="../img/trophy.png"><span class="info-coureur">'+element['categorie']+'</span><div class="right-panel"><span class="info-italic-coureur">Code INSEE : </span><span class="info-italic-coureur-value">'+element['code_insee']+'</span></div></div></div></div>';
         $('#runners').append(text);
     });
-    $('#runners').html(text);
     */
 }
 
