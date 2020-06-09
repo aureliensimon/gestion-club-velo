1bf8d3b95daddf6c81a75310ad0505ed12318be3
Aurelien Simon <aureliensimon29260@gmail.com>
2020-06-09 10:32:49 +0200

    add navbar & footer

---
+++ front/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="author" content="Aurélien SIMON - Aubane LECORCHET">
+        <title>Gestion club de vélo</title>
+
+        <!-- Bootstrap / JQuery -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    
+        <!-- CSS -->
+        <link rel="stylesheet" href="css/color.css">
+        <link rel="stylesheet" href="css/navbar.css">
+        <link rel="stylesheet" href="css/footer.css">
+    </head>
+    <body>
+        <?php include 'html/navbar.html';?>
+
+        <section id="authentification">
+            <form>
+                <label>Veuillez entrer votre adresse mail :</label>
+                <input type="email" id="mail" placeholder="exemple@test.com" autofocus required>
+                <input type="button" id="submit" value="Confirmer">
+            </form>
+        </section>
+
+        <?php include 'html/footer.html';?>
+    </body>
+</html>
