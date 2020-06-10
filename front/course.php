b2852c43af54d6292701afc4a63cb0ffaaedf443
Aurelien Simon <aureliensimon29260@gmail.com>
2020-06-10 10:44:45 +0200

    rename files

--- front/course.php
+++ front/course.php
@@ -6,18 +6,18 @@
         <title>Gestion des membres du club de vélo</title>
         
         <!-- CSS -->
-        <link rel="stylesheet" href="css/fiche-coureur.css">
+        <link rel="stylesheet" href="css/fiche-course.css">
 
         <!-- JS -->
         <script src="js/ajax.js" defer></script>
-        <script src="js/coureur.js" defer></script>
+        <script src="js/script.js" defer></script>
 
     </head>
     <body>
         <?php include 'html/navbar.html';?>
         <?php include 'html/fiche-coureur.html';?>
 
-        <section id="runners"></section>
+        <section id="liste-courses"></section>
 
         <?php include 'html/footer.html';?>
     </body>
---
+++ front/js/course.js
@@ -0,0 +1,13 @@
+function loadCourses (courses) {
+    let model = document.getElementById('fiche-course-modele');
+    let parent = document.getElementById('liste-courses');
+    
+    while (parent.firstChild) parent.removeChild(parent.firstChild);
+
+    courses.forEach(course => {
+        console.table(course);
+    });
+
+}
+
+ajaxRequest ('GET', '../api/courses.php', loadCourses);
