3c4f3006d83c095b798746ffa994a31266ce4176
S3yll <aubane.lecorchet@gmail.com>
2020-06-11 15:14:52 +0200

    delete files

--- front/js/course.js
+++ front/js/course.js
@@ -1,11 +1,4 @@
 function loadCourses (courses) {
-    /*
-    let model = document.getElementById('fiche-course-modele');
-    let parent = document.getElementById('liste-courses');
-    
-    while (parent.firstChild) parent.removeChild(parent.firstChild);
-    */
-
     courses.forEach(course => {
         let ficheCourse = $('#fiche-course-modele').clone();
 
