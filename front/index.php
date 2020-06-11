86e8659adfadafcf304c5cfa1b19290444ab2967
Aurelien Simon <aureliensimon29260@gmail.com>
2020-06-11 14:47:15 +0200

    remove include #quellebonneidee #maintenantlecodeesttellementplusclair #optimisation #clareté #phpsouscote #viemaviededevphp

--- front/index.php
+++ front/index.php
@@ -3,20 +3,68 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <?php include 'html/head.html';?>
-        
+        <meta charset="utf-8">
+        <meta name="author" content="Aurélien SIMON - Aubane LECORCHET">
+
+        <!-- FONTS -->
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet"> 
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;700;900&display=swap" rel="stylesheet"> 
+
+        <!-- Bootstrap / JQuery -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+        <!-- CSS -->
+        <link rel="stylesheet" href="css/color.css">
+        <link rel="stylesheet" href="css/navbar.css">
+        <link rel="stylesheet" href="css/footer.css">
+
         <title>Gestion club de vélo</title>
+
         <script src="js/ajax.js" defer></script>
-        <script src="js/session.js" defer></script>
         <script src="js/logout.js" defer></script>
+        <script src="js/session.js" defer></script>
         
     </head>
     <body>
-        <?php include 'html/navbar.html'; ?>
-        <?php include 'html/form-auth.html'; ?>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a class="navbar-brand" href="index.php">GestionVelo</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="coureur.php">Coureurs</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="course.php">Courses</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Compétitions</a>
+                </li>
+                </ul>
+            </div>
+        </nav>
+
+        <section id="authentification" style="display: none">
+            <form>
+                <label>Veuillez entrer votre adresse mail :</label>
+                <input type="email" id="user_mail" placeholder="exemple@test.com" autofocus required>
+                <input type="password" id="user_pwd" placeholder="password" autofocus required>
+                <input type="button" id="submit" value="Confirmer" onclick="setLogin()">
+            </form>
+        </section>
 
         <div id="content"></div>
-        
-        <?php include 'html/footer.html';?>
+
+        <footer class="page-footer font-small">
+            <div class="footer-copyright text-center py-3">© 2020 Copyright :
+            <a href="https://github.com/aureliensimon/gestion-club-velo">Aubane Lecorchet - Aurélien Simon</a>
+            </div>
+            <div><input type="submit" value="Se déconnecter" onclick="disconnectUser()"></div>
+        </footer>
     </body>
 </html>
