2321ef6c771c61949308c28092d0158f42fffcbf
S3yll <aubane.lecorchet@gmail.com>
2020-06-11 14:56:32 +0200

    Update index.php

--- front/index.php
+++ front/index.php
@@ -36,7 +36,7 @@
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav ml-auto">
-                <li class="nav-item active">
+                <li class="nav-item">
                     <a class="nav-link" href="coureur.php">Coureurs</a>
                 </li>
                 <li class="nav-item">
