cfe7251f3a4fe897fecab01aea0a72c0c6066b02
S3yll <aubane.lecorchet@gmail.com>
2020-06-12 10:37:36 +0200

    Revert "Merge branch 'master' of https://github.com/aureliensimon/gestion-club-velo"
    This reverts commit 6a1ae888d0607f5b2e2faaa80690c4fbb63f3043, reversing
    changes made to aa286626bd35827ae738fd108b7a1bdd10039ea4.

--- api/request.php
+++ api/request.php
@@ -76,14 +76,6 @@ if ($requestRessource == 'add_racing') {
     }
 }
 
-if ($requestRessource == 'getResult') {
-    if ($requestMethod == 'GET') {
-        $data = db_getResult($db);
-    } else {
-        $data="";
-    }
-}
-
 if ($data === false) {
     header('HTTP/1.1 400 Bad Request');
     exit();
--- install.sh
+++
@@ -1,3 +0,0 @@
-cp conf/prj-cir2-web-api.monposte.conf /etc/apache2/sites-available/prj-cir2-web-api.monposte.conf
-cp conf/prj-cir2-web-front.monposte.conf /etc/apache2/sites-available/prj-cir2-web-front.monposte.conf
-
